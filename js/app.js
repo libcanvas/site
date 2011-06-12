a5dd6411eb871691a412d42941e053f010a4624f
Alexander Rudenko <mur.mailbox@gmail.com>
2011-06-12 13:48:59 +0300

    Docs were added

---
+++ js/app.js
@@ -0,0 +1,8 @@
+atom.dom(function() {
+    atom.dom('#hello_canvas_button').bind({click: Doc.helloCanvas});
+
+    Doc.init();
+
+    hljs.tabReplace = '    ';
+    hljs.initHighlightingOnLoad();
+});
